d367067f10a3921f7b20be5d5d4b7f7a1eeec3d9
murf <36916060+MurfCode@users.noreply.github.com>
2018-05-23 21:54:37 +0100

    we’re done i thonk

--- src/core/managers/ManipulationManager.js
+++ src/core/managers/ManipulationManager.js
@@ -2,9 +2,10 @@ const { Canvas } = require('canvas-constructor');
 const { registerFont } = require('canvas');
 const { readFile } = require('fs-nextra');
 registerFont('./assets/fonts/Futura.otf', { family: 'balance' });
+registerFont('./assets/fonts/discord.ttf', { family: 'b1nzy' });
 
 module.exports = (bot) => ({
-  balance: async function (member, amount, channelID) {
+  balance: async function (msg, member, amount, channelID) {
     let res = await readFile('./assets/images/balance.jpg');
     let img = new Canvas(2048, 779)
     .addImage(res, 0, 0, 2048, 779)
@@ -13,8 +14,40 @@ module.exports = (bot) => ({
     .addText(`${member.username}#${member.discriminator}`, 19, 351)
     .addText(`¥ ${amount}`, 19, 550)
     .toBuffer();
-    bot.createMessage(channelID, `<:ui_paper:447352551377993738> **${member.username}#${member.discriminator}**'s balance`, {
-      file: img, name: 'balance.jpg' 
+    bot.createMessage(channelID, msg.__('commands.balance.content', { 
+      username: member.username, 
+      discrim: member.discriminator 
+    }), {
+      file: img,
+      name: 'balance.jpg'
+    });
+  },
+  
+  profile: async function (msg, member, channelID) {
+    let pmDB = await bot.db.fetch(`profile_${member.id}`);
+    let res = await readFile('./assets/images/profile.jpg');
+    let img = new Canvas(1553, 1536)
+    .toBuffer();
+    bot.createMessage(channelID, msg.__('commands.profile.content', {
+      username: member.username,
+      discrim: member.discriminator
+    }), {
+      file: img,
+      name: 'profile.jpg'
+    });
+  },
+  
+  b1nzy: async function (str, channelID) {
+    let res = await readFile('./assets/images/b1nzy.png');
+    let img = new Canvas(489, 109)
+    .addImage(res, 0, 0, 489, 109)
+    .setTextFont('16px b1nzy')
+    .setColor('#FFFFFF')
+    .addText(str, 80, 25, 450)
+    .toBuffer();
+    bot.createMessage(channelID, '', {
+      file: img,
+      name: 'bad-b1nzy.png'
     });
   }
 });
